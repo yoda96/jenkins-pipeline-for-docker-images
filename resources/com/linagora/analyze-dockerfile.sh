baef8d598a9ee89a88987dc970d69309da12407c
Vincent Zurczak <vincent.zurczak@gmail.com>
2018-11-09 14:41:00 +0100

    Handle the governance SL

---
+++ resources/com/linagora/analyze-dockerfile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "You can apply some linting rules"
+echo "See https://github.com/hadolint/hadolint"
+echo ""
+echo "You can also parse the Dockerfile..."
+echo "Or verify the history of an image by instantiating it and using docker history."
+echo "See https://docs.docker.com/engine/reference/commandline/history/"
+echo ""
+echo "You can collect information and output them in a file (e.g. /tmp/gov.results.txt) before sending them."
+
